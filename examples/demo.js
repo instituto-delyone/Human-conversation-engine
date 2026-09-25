63be1f648446d1bd8e6c24b4274a201723ba7ff1
Instituto Delyone de Medicina e Tecnologia <instituto@delyone.com>
2026-09-25 05:15:19 -0300

    Use deterministic conversational provider in demo

--- examples/demo.js
+++ examples/demo.js
@@ -1,9 +1,9 @@
 import {ConversationEngine} from "../src/core/conversation-engine.js";
-import {EchoProvider} from "../src/providers/provider.js";
+import {RuleBasedProvider} from "../src/providers/provider.js";
 
 const engine=new ConversationEngine({
-  provider:new EchoProvider(),
+  provider:new RuleBasedProvider(),
   logger:event=>console.log("[event]",event.event)
 });
-for(const message of ["Oi!","A gente pode continuar aquele projeto?","Não, não foi isso que eu quis dizer."])
+for(const message of ["Oi!","A gente pode continuar aquele projeto?","Não, não foi isso que eu quis dizer.","Quero mudar de assunto."])
   console.log(">",message,"\n<",(await engine.handle(message)).response);
